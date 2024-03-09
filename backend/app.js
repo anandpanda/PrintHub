6e91cc811708c3eceb7ca5a166131471893b4591
Anand Panda <anand612p@gmail.com>
2024-03-10 03:13:41 +0530

    login signup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
 const app = express();
 
 const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
